152fd1384c26509fadf8df9018145494c79a404e
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-30 23:41:59 +0100

    Installed package to get app version

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,16 +1,27 @@
 <template>
   <q-page class="flex flex-center column">
-    <h6>info page</h6>
-    
+    {{ state.version }}
   </q-page>
 </template>
 
 <script>
+import { onMounted, reactive } from "vue";
 export default {
+  name: "Info",
+  setup() {
+    const state = reactive({
+      version: "",
+    });
 
-}
+    onMounted(() => {
+      cordova.getAppVersion.getVersionNumber(function (version) {
+        state.version = version;
+      });
+    });
+
+    return { state };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
