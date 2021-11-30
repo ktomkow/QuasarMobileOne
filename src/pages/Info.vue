cd33bbe513da93e613b7a39ff83d662ffe801922
ktomkow <krzysztof.tomkow@gmail.com>
2021-11-30 23:50:13 +0100

    Show application version and name

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -1,6 +1,25 @@
 <template>
   <q-page class="flex flex-center column">
-    {{ state.version }}
+    <q-markup-table>
+      <tbody>
+        <tr>
+          <td>app name</td>
+          <td>{{state.appName}}</td>
+        </tr>
+        <tr>
+          <td>package name</td>
+          <td>{{state.packageName}}</td>
+        </tr>
+        <tr>
+          <td>version code</td>
+          <td>{{state.versionCode}}</td>
+        </tr>
+        <tr>
+          <td>version number</td>
+          <td>{{state.versionNumber}}</td>
+        </tr>
+      </tbody>
+    </q-markup-table>
   </q-page>
 </template>
 
@@ -10,12 +29,24 @@ export default {
   name: "Info",
   setup() {
     const state = reactive({
-      version: "",
+      appName: "",
+      packageName: "",
+      versionCode: "",
+      versionNumber: ""
     });
 
     onMounted(() => {
-      cordova.getAppVersion.getVersionNumber(function (version) {
-        state.version = version;
+      cordova.getAppVersion.getAppName(function (x) {
+        state.appName = x;
+      });
+      cordova.getAppVersion.getPackageName(function (x) {
+        state.packageName = x;
+      });
+      cordova.getAppVersion.getVersionCode(function (x) {
+        state.versionCode = x;
+      });
+      cordova.getAppVersion.getVersionNumber(function (x) {
+        state.versionNumber = x;
       });
     });
 
